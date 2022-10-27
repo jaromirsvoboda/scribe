6124a6a65046c8cd86e9334a5fd6333d6858f74a
jsvoboda <jaromirsvoboda@email.cz>
2022-10-27 22:40:41 +0200

    Soup init

--- src/scribe/__main__.py
+++ src/scribe/__main__.py
@@ -1,3 +1,4 @@
+from importlib.resources import path
 import logging
 import time
 import click
@@ -18,37 +19,12 @@ logging.basicConfig(
 logging.getLogger().addHandler(logging.StreamHandler())
 logging.info(f"Starting Scribe at {timestamp}.")
 
-
-
-
 @click.command()
-@click.option('--count', default=1, help='Number of greetings.')
-@click.option('--name', prompt='Your name',
-              help='The person to greet.')
-def hello(count, name):
-    """Simple program that greets NAME for a total of COUNT times."""
-    for x in range(count):
-        click.echo(f"Hello {name}!")
+@click.option('--path', default=".", help='Path to the folder containing kindle notes (in .html format)')
+def main(path):
+    print(path)
+    scribe = Scribe()
+    scribe.run(path)
 
 if __name__ == '__main__':
-    hello()  # type: ignore
-
-# `cwd`: current directory is straightforward
-cwd = Path.cwd()
-
-# `mod_path`: According to the accepted answer and combine with future power
-# if we are in the `helper_script.py`
-mod_path = Path(__file__).parent
-# OR if we are `import helper_script`
-# mod_path = Path(scribe.__file__).parent
-
-# `src_path`: with the future power, it's just so straightforward
-relative_path_1 = 'same/parent/with/helper/script/'
-relative_path_2 = '../../or/any/level/up/'
-src_path_1 = (mod_path / relative_path_1).resolve()
-src_path_2 = (mod_path / relative_path_2).resolve()
-
-
-
-scribe = Scribe()
-scribe.run()
+    main()  # type: ignore
--- src/scribe/scribe.py
+++ src/scribe/scribe.py
@@ -1,5 +1,15 @@
+import os
+from timeit import default_timer
+
+from bs4 import BeautifulSoup
 
 
 class Scribe():
-    def run(self):
-        print("Hello World!")
+    def run(self, root_directory_path: str):
+        start = default_timer()
+        for root, dirs, files in os.walk(root_directory_path):
+            html_files = [file for file in files if file.endswith(".html")]
+            for html_file in html_files:
+                with open(os.path.join(root, html_file)) as fp:
+                    soup = BeautifulSoup(fp, 'html.parser')
+                    print(soup)
